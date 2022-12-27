01a337a809169ad11e8afb4b786329319155042a
Walker Le <walker.le88@gmail.com>
2022-12-27 16:44:48 -0700

    commit tree aef0fa6f8095d190eca0b0f428742c03bdee6732

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+const namesArray = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = (array) => {
+    const newArray = array.slice(0, 2);
+
+    return newArray;
+}
+
+const returnLastTwoDrivers = (array) => {
+    const newArray = array.slice(-2);
+    
+    return newArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function (value) {
+        return multiplier * value;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+// function selectDifferentDrivers(array, driverFunction) {
+//     if (driverFunction == returnFirstTwoDrivers) {
+//         return returnFirstTwoDrivers(array);
+//     } else {driverFunction == returnLastTwoDrivers
+//         return returnLastTwoDrivers(array);
+//     }
+// }
+
+function selectDifferentDrivers(array, driverFunction) {
+    return driverFunction(array);
+}
+
+console.log(selectDifferentDrivers(namesArray, returnLastTwoDrivers));
